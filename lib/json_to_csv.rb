120aadefc9a03d2bbab9f8806d64e7035091ab3e
Marine <marine.schmitt.7@gmail.com>
2021-05-16 22:24:41 +0200

    add more tests + final refactoring

--- lib/json_to_csv.rb
+++ lib/json_to_csv.rb
@@ -7,50 +7,65 @@ require 'open-uri'
 module JsonToCSV
   class Converter
 
+
+    # Function that takes a json path or link and parses it
     private_class_method def self.parse_json(json_path)
-                            json_file = open(json_path) { |f| f.read }
-                            parsed_json = JSON.parse(json_file)
-                            unless parsed_json.is_a? Array
-                              raise TypeError.new "Input JSON must be an array of objects"
-                            end
-                            parsed_json
-                         end
+      json_file = open(json_path) { |f| f.read }
+      parsed_json = JSON.parse(json_file)
+      unless parsed_json.is_a? Array
+        raise TypeError.new "Input JSON must be an array of objects"
+      end
+      parsed_json
+    end
 
 
+    # Function that takes a json object and generates the list of this object's attribute names
     private_class_method def self.get_csv_header(json, parent=nil)
-                           json.each_with_object([]) do |(k,v), header|
-                             v_is_hash = v.is_a? Hash
-                             key_with_parent = parent ? "#{parent}.#{k}" : k
-                             if v_is_hash
-                               header.concat(get_csv_header(v, key_with_parent))
-                             else
-                               header << key_with_parent
-                             end
-                           end
-                         end
+     json.each_with_object([]) do |(k,v), header|
+       # get the "profiles.facebook.id" format style
+       key_with_parent = parent ? "#{parent}.#{k}" : k
+       if v.is_a? Hash
+         header.concat(get_csv_header(v, key_with_parent))
+       else
+         header << key_with_parent
+       end
+     end
+    end
 
 
+    # Function that takes a json object and generates the list of this object's attribute values
     private_class_method def self.get_csv_row(json)
-                           json.each_with_object([]) do |(k,v), row|
-                             v_is_hash = v.is_a? Hash
-                             if v_is_hash
-                               row.concat(get_csv_row(v))
-                             elsif v.is_a? Array
-                               row << v.join(',')
-                             else
-                               row << v
-                             end
-                           end
-                         end
+     json.each_with_object([]) do |(k,v), row|
+       if v.is_a? Hash
+         row.concat(get_csv_row(v))
+       elsif v.is_a? Array
+         row << v.join(',')
+       else
+         row << v
+       end
+     end
+    end
 
+
+    # Main function, takes an input json path (file or link) and generates an output csv file in the given path
     def self.json_to_csv(json_path, csv_path)
       CSV.open(csv_path, "w") do |csv|
-        users_json = parse_json(json_path)
-        users_json.each do |row|
-          if row['id'] == 0
-            csv << get_csv_header(row)
+        json = parse_json(json_path)
+        header = ''
+        json.each_with_index do |row, k|
+          row_header = get_csv_header(row)
+
+          # append the csv header from the first json object attributes
+          if k == 0
+            csv << row_header unless row_header.empty?
+            header = row_header
+
+          # test if object has extra/missing/different attributes
+          elsif row_header != header
+            raise JSON::ParserError.new "JSON objects must follow the same schema"
           end
-          csv << get_csv_row(row)
+
+          csv << get_csv_row(row) unless row.empty?
         end
       end
     end
--- test/json_to_csv_test.rb
+++ test/json_to_csv_test.rb
@@ -10,10 +10,10 @@ class JsonToCsvTest < Test::Unit::TestCase
   FILE_LINKS = {'json' => "https://gist.githubusercontent.com/romsssss/6b8bc16cfd015e2587ef6b4c5ee0f232/raw/f74728a6ac05875dafb882ae1ec1deaae4d0ed4b/users.json",
                 'csv' => "https://gist.githubusercontent.com/romsssss/2efc2ace305b98be85d0fe617a10ac8b/raw/a43cffb5dc2170294e9635207f18bacba2b68001/users.csv"}
 
-  GENERATED_CSV_PATH = "../csv/generated_users.csv"
+  GENERATED_CSV_PATH = "../csv/output.csv"
   INPUT_JSON_PATH = "../json/users.json"
 
-  # Test with json and csv examples from the task description
+  # Test with json and csv examples from the task description (file + url)
   def test_json_to_csv
     JsonToCSV::Converter.json_to_csv(FILE_LINKS['json'], GENERATED_CSV_PATH)
     generated_csv = open(GENERATED_CSV_PATH) { |f| f.read }
@@ -25,13 +25,10 @@ class JsonToCsvTest < Test::Unit::TestCase
     assert_equal generated_csv, gold_csv
   end
 
-  # Test with misshaped or empty or invalid json input
+  # Test with misshaped or invalid json input
   def test_misshaped_json
     assert_raise JSON::ParserError do
       JsonToCSV::Converter.json_to_csv("../json/misshaped.json", GENERATED_CSV_PATH)
-    end
-    assert_raise JSON::ParserError do
-      JsonToCSV::Converter.json_to_csv("../json/empty.json", GENERATED_CSV_PATH)
     end
     assert_raise JSON::ParserError do
       JsonToCSV::Converter.json_to_csv("https://www.google.fr", GENERATED_CSV_PATH)
@@ -40,5 +37,39 @@ class JsonToCsvTest < Test::Unit::TestCase
       JsonToCSV::Converter.json_to_csv("../json/not_a_list.json", GENERATED_CSV_PATH)
     end
   end
+
+  # Test with not all objects following the same schema
+  def test_different_attributes
+    assert_raise JSON::ParserError do
+      JsonToCSV::Converter.json_to_csv("../json/different_attribute.json", GENERATED_CSV_PATH)
+    end
+    assert_raise JSON::ParserError do
+      JsonToCSV::Converter.json_to_csv("../json/extra_attribute.json", GENERATED_CSV_PATH)
+    end
+    assert_raise JSON::ParserError do
+      JsonToCSV::Converter.json_to_csv("../json/missing_attribute.json", GENERATED_CSV_PATH)
+    end
+  end
+
+  # Test with empty file/array/json object/attribute
+  def test_empty_element
+    assert_raise JSON::ParserError do
+      JsonToCSV::Converter.json_to_csv("../json/empty.json", GENERATED_CSV_PATH)
+    end
+
+    JsonToCSV::Converter.json_to_csv("../json/empty_array.json", GENERATED_CSV_PATH)
+    generated_csv = open(GENERATED_CSV_PATH) { |f| f.read }
+    gold_csv  = open("../csv/empty.csv") {|f| f.read }
+    assert_equal generated_csv, gold_csv
+
+    JsonToCSV::Converter.json_to_csv("../json/empty_object.json", GENERATED_CSV_PATH)
+    generated_csv = open(GENERATED_CSV_PATH) { |f| f.read }
+    assert_equal generated_csv, gold_csv
+
+    assert_raise JSON::ParserError do
+      JsonToCSV::Converter.json_to_csv("../json/one_empty_object.json", GENERATED_CSV_PATH)
+    end
+  end
+
 end
 
